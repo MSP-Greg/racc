0bfc80069417cf21b957302089185ad847fbc996
MSP-Greg <MSP-Greg@users.noreply.github.com>
2019-05-14 16:03:10 -0500

    run_tests.sh - don't install RubyGems 2.6.12...

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -6,7 +6,6 @@ test_frozen_strings=$(ruby -e 'puts ((RUBY_ENGINE == "ruby" || RUBY_ENGINE == "j
 
 if [[ $test_frozen_strings == "true" ]] ; then
   echo "NOTE: enabling frozen string literals"
-  rvm install rubygems 2.6.12 --force # because of an issue in rubygems 2.7 with ruby 2.5 and frozen string literals
   export RUBYOPT="--enable-frozen-string-literal --debug=frozen-string-literal"
 fi
 
